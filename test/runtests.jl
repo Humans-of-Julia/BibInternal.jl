ddb2e33ea84e8d9682163691cbe59c555d7c2f8d
Daniel Ingraham <d.j.ingraham@gmail.com>
2021-08-02 20:33:24 -0400

    Add more `Name` junior parsing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -50,7 +50,7 @@ end
 
 end
 
-@testset "Name" begin
+@testset "junior parsing for BibInternal.Name" begin
     name = BibInternal.Name("Doe, Jr, Abe Brian")
     name_expected = BibInternal.Name("", "Doe", "Jr", "Abe", "Brian")
     @test name == name_expected
@@ -59,4 +59,10 @@ end
     name_expected = BibInternal.Name("", "Doe", "Jr.", "A.", "B.")
     @test name == name_expected
 
+    # If no comma is used between the last name and junior, the junior should be
+    # combined with the last name
+    # (https://nwalsh.com/tex/texhelp/bibtx-23.html).
+    name = BibInternal.Name("Doe Jr., A. B.")
+    name_expected = BibInternal.Name("", "Doe Jr.", "", "A.", "B.")
+    @test name == name_expected
 end
