f35a6b5287b7f1fd31dd18338ddbb87fdb13f4f5
Michael Goerz <mail@michaelgoerz.net>
2026-03-05 14:03:56 -0500

    Make JET optional

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,22 @@ using BibInternal
 
 using Aqua
 using ExplicitImports
-using JET
 using Test
 using TestItemRunner
+
+const JET_AVAILABLE = try
+    using JET
+    true
+catch
+    false
+end
+@show JET_AVAILABLE
 
 @testset "Package tests: BibInternal" begin
     include("Aqua.jl")
     include("ExplicitImports.jl")
-    include("JET.jl")
+    if JET_AVAILABLE
+        include("JET.jl")
+    end
     include("TestItemRunner.jl")
 end
